17bb35aef423b5577bf6e017540ff34589b7da89
raidsnail <raidsnail@raidsnail_PC>
2016-05-31 19:59:50 +0200

    general operators (~,a,c,d,i,b) and calculator

--- aufgabe1/Main.java
+++ aufgabe1/Main.java
@@ -1,9 +1,20 @@
 package aufgabe1;
 
+import test.Calculator;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Calculator Test
+		
+
+				Calculator calc = new Calculator();
+				
+				calc.setInputStream("4 5~6c ~78 ~9cw");
+				calc.executeInput();		
+				System.out.println(calc.getOutputStream());
 
 	}
 
